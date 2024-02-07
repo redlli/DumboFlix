b3b71fdf1ffce1e1b456ab85483b11b79e5abfb4
“redlli” <thegaikwadamardeep@gmail.com>
2024-02-07 13:03:37 +0530

    Initial commit

---
+++ main/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.svg" className="App-logo" alt="o" />
+        <p>
+          DumboFlix
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
